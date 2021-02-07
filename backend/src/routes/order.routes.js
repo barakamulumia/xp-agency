5c3723b8adbc22d669ec4639552f023279599850
Baraka Mulumia <barakamulumia@gmail.com>
2021-02-07 14:39:19 +0300

    ^ moved  allow headers function to server

--- backend/src/routes/order.routes.js
+++ backend/src/routes/order.routes.js
@@ -1,15 +1,7 @@
-const { orderWare } = require("../middlewares");
+const controller = require("../controllers/order.controller");
 
 module.exports = function (app) {
-  app.use(function (_req, res, next) {
-    res.header(
-      "Access-Control-Allow-Headers",
-      "x-access-token, Origin, Content-Type, Accept"
-    );
-    next();
-  });
+  app.get("/api/orders/client", controller.getAllOrdersByUserId);
 
-  app.get("/api/orders/all", orderWare.getAllOrdersByUserId);
-
-  app.post("/api/orders/create", orderWare.creteOrder);
+  app.post("/api/orders/create", controller.creteOrder);
 };
