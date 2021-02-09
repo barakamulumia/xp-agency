d868f71acc44703c7d076d4f9f07cffda1053e60
Baraka Mulumia <barakamulumia@gmail.com>
2021-02-09 14:07:28 +0300

    client page grid

---
+++ client/src/pages/Client/client.elements.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import { Grid } from "@material-ui/core";
+
+export const Box = styled(Grid)`
+  background: url("https://i.postimg.cc/BvnqxGPg/house-packing.jpg");
+  background-size: cover;
+  min-height: 100vh;
+  background-position: center;
+`;
