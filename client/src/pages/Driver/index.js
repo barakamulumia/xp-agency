01c6f9ffe846c786d23774ef6c290375d4f7f57e
Baraka Mulumia <barakamulumia@gmail.com>
2021-02-09 14:11:10 +0300

    driver reg form xpress

--- client/src/pages/Driver/index.js
+++ client/src/pages/Driver/index.js
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from "react";
-import { Navbar } from "../../components";
+import destination from "../../images/Destination.svg";
+import { Navbar, XpressRegForm, Image } from "../../components";
 import UserService from "../../services/user.service";
+import { Container } from "../../Resources/Styles/global";
+import { Box } from "./driver.elements";
+import { Grid } from "@material-ui/core";
 
 export default function Driver() {
   const [content, setContent] = useState(undefined);
+
   useEffect(() => {
     UserService.getDriverBoard().then(
       (response) => {
@@ -21,9 +26,18 @@ export default function Driver() {
     );
   }, []);
   return (
-    <div>
-      <Navbar />
-      {content && <h1>{content}</h1>}
-    </div>
+    <Box>
+      <Container>
+        <Navbar />
+        <Grid container spacing={2} justify="center" alignContent="center">
+          <Grid item xs={12} sm={6} md={6}>
+            <XpressRegForm />
+          </Grid>
+          <Grid item xs={12} sm={6} md={6}>
+            <Image start="true" alt="my current location" src={destination} />
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
   );
 }
