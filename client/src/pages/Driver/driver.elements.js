3e6e50d303c089e8addd13ca00a0a8faa8e90f75
Baraka Mulumia <barakamulumia@gmail.com>
2021-02-09 14:10:06 +0300

    driver grid

---
+++ client/src/pages/Driver/driver.elements.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import { Grid } from "@material-ui/core";
+
+export const Box = styled(Grid)`
+  background: url("https://i.postimg.cc/DZqKmp8k/highway-truck.jpg");
+  background-size: cover;
+  min-height: 100vh;
+`;
