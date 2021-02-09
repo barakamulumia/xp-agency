bc9bce17b158f0e3bc496a0edc40ab56f4018c7e
Baraka Mulumia <barakamulumia@gmail.com>
2021-02-09 14:02:48 +0300

    ^ input width 100%

--- client/src/components/Map/mapinput.elements.js
+++ client/src/components/Map/mapinput.elements.js
@@ -3,7 +3,7 @@ import { TextField } from "@material-ui/core";
 
 export const LocationSearchInput = styled(TextField)`
   height: 60px;
-  width: 30%;
+  width: 100%;
   border-radius: 5px;
   margin: 5px 20px;
   font-size: 1.2em;
