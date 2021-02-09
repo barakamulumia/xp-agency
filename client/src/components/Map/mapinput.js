058b2849e725ddc43a080ecfaaae9eac63f0b769
Baraka Mulumia <barakamulumia@gmail.com>
2021-02-09 14:04:16 +0300

    ^ changes class component to functional

--- client/src/components/Map/mapinput.js
+++ client/src/components/Map/mapinput.js
@@ -1,94 +1,70 @@
 import { v4 as uuidv4 } from "uuid";
-import React, { Component } from "react";
+import React, { useState } from "react";
 import PlacesAutocomplete, {
   geocodeByAddress,
   getLatLng,
 } from "react-places-autocomplete";
 
 import { LocationSearchInput } from "./mapinput.elements";
+export default function MapSearchInput({ SET_LOCATION }) {
+  const [address, setAddress] = useState("");
 
-class MapSearchInput extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      address: "",
-      placeId: "",
-      latLng: "",
-    };
-  }
-
-  handleChange = (address) => {
-    this.setState({ address });
+  const handleChange = (address) => {
+    setAddress(address);
   };
 
-  handleSelect = (address) => {
-    geocodeByAddress(address)
-      .then((results) => {
-        this.setState({
-          address: results[0].formatted_address,
-          placeId: results[0].place_id,
-        });
-        return getLatLng(results[0]);
-      })
-      .then((latLng) => {
-        this.setState({
-          latLng,
-        });
-      })
-      .catch((error) => console.error("Error", error));
+  const handleSelect = async (selectedAddress) => {
+    const results = await geocodeByAddress(selectedAddress);
+    const latlng = await getLatLng(results[0]);
+    setAddress(results[0].formatted_address);
+    SET_LOCATION({
+      address: results[0].formatted_address,
+      latlng,
+      placeId: results[0].place_id,
+    });
   };
 
-  render() {
-    console.log(this.state);
-    return (
-      <div>
-        <PlacesAutocomplete
-          value={this.state.address}
-          onChange={this.handleChange}
-          onSelect={this.handleSelect}
-        >
-          {({
-            getInputProps,
-            suggestions,
-            getSuggestionItemProps,
-            loading,
-          }) => (
-            <div>
-              <LocationSearchInput
-                {...getInputProps({
-                  placeholder: "Search Places ...",
-                  className: "location-search-input",
-                })}
-              />
-              <div className="autocomplete-dropdown-container">
-                {loading && <div>Loading...</div>}
-                {suggestions.map((suggestion) => {
-                  const className = suggestion.active
-                    ? "suggestion-item--active"
-                    : "suggestion-item";
-                  // inline style for demonstration purpose
-                  const style = suggestion.active
-                    ? { backgroundColor: "#fafafa", cursor: "pointer" }
-                    : { backgroundColor: "#ffffff", cursor: "pointer" };
-                  return (
-                    <div
-                      key={uuidv4()}
-                      {...getSuggestionItemProps(suggestion, {
-                        className,
-                        style,
-                      })}
-                    >
-                      <span>{suggestion.description}</span>
-                    </div>
-                  );
-                })}
-              </div>
+  return (
+    <div>
+      <PlacesAutocomplete
+        value={address}
+        onChange={handleChange}
+        onSelect={handleSelect}
+      >
+        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+          <div>
+            <LocationSearchInput
+              {...getInputProps({
+                placeholder: "Search Places ...",
+                className: "location-search-input",
+              })}
+            />
+            <div className="autocomplete-dropdown-container">
+              {loading && <div>Loading...</div>}
+              {suggestions.map((suggestion) => {
+                const className = suggestion.active
+                  ? "suggestion-item--active"
+                  : "suggestion-item";
+                // inline style for demonstration purpose
+                const style = suggestion.active
+                  ? { backgroundColor: "#fafafa", cursor: "pointer" }
+                  : { backgroundColor: "#ffffff", cursor: "pointer" };
+                return (
+                  <div
+                    key={uuidv4()}
+                    {...getSuggestionItemProps(suggestion, {
+                      className,
+                      style,
+                    })}
+                  >
+                    <span>{suggestion.description}</span>
+                  </div>
+                );
+              })}
             </div>
-          )}
-        </PlacesAutocomplete>
-      </div>
-    );
-  }
+          </div>
+        )}
+      </PlacesAutocomplete>
+    </div>
+  );
 }
-
-export default MapSearchInput;
