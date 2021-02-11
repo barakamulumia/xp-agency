ec4ebf434d2756d4c28d1c99e38ee32b90420a3b
Baraka Mulumia <barakamulumia@gmail.com>
2021-02-11 16:09:48 +0300

    +client display data

---
+++ client/src/Resources/Data/clientinfo.js
@@ -0,0 +1,22 @@
+import image from "../../images/nav.svg";
+
+export const ClientInfo = {
+  topline: {
+    content: "Let us bring your products to the world",
+  },
+  headline: {
+    content: "We live for the art of moving",
+  },
+  description: {
+    content:
+      "Deliver as you wish with our efficient safe and reliable moving resources. We provide product moving solutions to help you handle your personal mobility services.",
+  },
+  btn: {
+    label: "Move Products Within Seconds",
+  },
+  img: {
+    src: image,
+    alt: "Town Svg Image",
+    start: "",
+  },
+};
