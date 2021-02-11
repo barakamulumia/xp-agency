ed04e1f973387404a3cb537a05a72fc03c703e18
Baraka Mulumia <barakamulumia@gmail.com>
2021-02-11 16:12:56 +0300

    ^ setting active index for order details

--- client/src/pages/Client/index.js
+++ client/src/pages/Client/index.js
@@ -1,6 +1,16 @@
 import React, { useState, useEffect } from "react";
+import { Redirect } from "react-router-dom";
+
 import UserService from "../../services/user.service";
-import { Navbar, OrderCard } from "../../components";
+import {
+  Navbar,
+  OrderCard,
+  OrderDetails,
+  ClientInfoSection,
+  Footer,
+} from "../../components";
+import { ClientInfo } from "../../Resources/Data/clientinfo";
+
 import {
   Box,
   OrderCardContainer,
@@ -15,11 +25,17 @@ import {
   MdImportExport,
   MdCheck,
 } from "react-icons/md";
+import { Grid } from "@material-ui/core";
 // import { Grid } from "@material-ui/core";
 
 export default function Client() {
   const [user, setUser] = useState(undefined);
 
+  const [activeId, setActiveId] = useState(undefined);
+
+  const setActiveIndex = (id) => {
+    setActiveId(id);
+  };
   useEffect(() => {
     UserService.getClientBoard().then(
       (response) => {
@@ -37,42 +53,72 @@ export default function Client() {
     );
   }, []);
 
-  console.log(user);
+  if (typeof user !== "object") {
+    return <Redirect to="/" />;
+  }
 
   return (
-    <Box>
+    <div
+      style={{
+        backgroundColor: "#fdfdf8",
+      }}
+    >
       <Container>
         <Navbar />
-        <OrderCardContainer>
-          <OrderCardHeader>My Trips</OrderCardHeader>
-          <OrdersNav>
-            <OrderLink variant="contained" startIcon={<MdAccessAlarm />}>
-              Pending
-            </OrderLink>
-            <OrderLink
-              variant="contained"
-              color="secondary"
-              startIcon={<MdWarning />}
-            >
-              Cancelled
-            </OrderLink>
-            <OrderLink variant="contained" startIcon={<MdImportExport />}>
-              InProgress
-            </OrderLink>
-            <OrderLink
-              variant="contained"
-              color="primary"
-              startIcon={<MdCheck />}
-            >
-              Successfull
-            </OrderLink>
-          </OrdersNav>
-          {user &&
-            user.orders.map((order) => (
-              <OrderCard order={order} key={order._id} />
-            ))}
-        </OrderCardContainer>
+        <ClientInfoSection {...ClientInfo} />
       </Container>
-    </Box>
+      <Box container justify="space-between">
+        <Container>
+          <Grid container justify="space-between">
+            <Grid item>
+              <OrderCardContainer>
+                <OrderCardHeader>My Trips</OrderCardHeader>
+                <OrdersNav>
+                  <OrderLink variant="contained" startIcon={<MdAccessAlarm />}>
+                    Pending
+                  </OrderLink>
+                  <OrderLink
+                    variant="contained"
+                    color="secondary"
+                    startIcon={<MdWarning />}
+                  >
+                    Cancelled
+                  </OrderLink>
+                  <OrderLink variant="contained" startIcon={<MdImportExport />}>
+                    InProgress
+                  </OrderLink>
+                  <OrderLink
+                    variant="contained"
+                    color="primary"
+                    startIcon={<MdCheck />}
+                  >
+                    Successfull
+                  </OrderLink>
+                </OrdersNav>
+                {user &&
+                  user.orders.map((order) => (
+                    <OrderCard
+                      order={order}
+                      key={order._id}
+                      SET_ACTIVE_INDEX={setActiveIndex}
+                    />
+                  ))}
+              </OrderCardContainer>
+            </Grid>
+            <Grid item>
+              {user && (
+                <OrderDetails
+                  order={
+                    user.orders.find((order) => order._id === activeId) ||
+                    user.orders[0]
+                  }
+                />
+              )}
+            </Grid>
+          </Grid>
+        </Container>
+      </Box>
+      <Footer />
+    </div>
   );
 }
