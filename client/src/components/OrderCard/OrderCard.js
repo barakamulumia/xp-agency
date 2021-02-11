8e20637682af80a20635be6c5552de7b35174420
Baraka Mulumia <barakamulumia@gmail.com>
2021-02-11 16:09:25 +0300

    setting active index

--- client/src/components/OrderCard/OrderCard.js
+++ client/src/components/OrderCard/OrderCard.js
@@ -4,36 +4,8 @@ import {
   OrderCardHeader,
   OrderDestination,
 } from "./OrderCard.elements";
-// const sampleOrder = {
-//   _id: "60239067af951507404f8cbf",
-//   pickup: {
-//     latlng: {
-//       lat: -1.262258,
-//       lng: 36.8069072,
-//     },
-//     address:
-//       "Mpaka Rd, West Point Building, 6th Floor, Parklands Nairobi KE, Nairobi, Kenya",
-//     placeId: "ChIJOxeCyNYQLxgR97ZoGN0GJQc",
-//   },
-//   destination: {
-//     latlng: {
-//       lat: -1.2715319,
-//       lng: 36.8204853,
-//     },
-//     address: "Stima Plaza, Nairobi, Kenya",
-//     placeId: "ChIJz3yHGyYXLxgR0fXysDZ-NNI",
-//   },
-//   load: "Designated",
-//   status: "pending",
-//   dateTime: "2021-02-10T07:47:11.872Z",
-//   moveType: "fr",
-//   clientId: "601667e4d14c2715888e4623",
-//   driverId: "601fc95aa69bba33f4e2ad58",
-//   charges: 4000,
-//   __v: 0,
-// };
 
-const OrderCard = ({ order }) => {
+const OrderCard = ({ order, SET_ACTIVE_INDEX }) => {
   const formatMoveType = (moveType) =>
     moveType === "hm"
       ? "House Moving"
@@ -42,7 +14,7 @@ const OrderCard = ({ order }) => {
       : "Freight";
 
   return (
-    <OrderContainer elevation={6}>
+    <OrderContainer elevation={6} onClick={() => SET_ACTIVE_INDEX(order._id)}>
       <OrderCardHeader>{formatMoveType(order.moveType)}</OrderCardHeader>
       <OrderDestination>{order.destination.address}</OrderDestination>
     </OrderContainer>
