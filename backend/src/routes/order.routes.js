4c75460bc72e91c32cbeb33aaacac44aaaae94b7
Baraka Mulumia <barakamulumia@gmail.com>
2021-02-13 08:54:29 +0300

    @ renamed route to user

--- backend/src/routes/order.routes.js
+++ backend/src/routes/order.routes.js
@@ -1,7 +1,7 @@
 const controller = require("../controllers/order.controller");
 
 module.exports = function (app) {
-  app.get("/api/orders/client", controller.getAllOrdersByUserId);
+  app.get("/api/orders/user", controller.getAllOrdersByUserId);
 
   app.post("/api/orders/create", controller.creteOrder);
 };
