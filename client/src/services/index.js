39512bd8566d7298539cc2fbdc9309909ddd245e
Baraka Mulumia <barakamulumia@gmail.com>
2021-02-16 16:41:58 +0300

    Refactoring for optimization

---
+++ client/src/services/index.js
@@ -0,0 +1,5 @@
+export { default as UserService } from "./user.service";
+export { default as OrderService } from "./order.service";
+export { default as AuthService } from "./auth.service";
+export { default as DriverService } from "./driver.service";
+export { default as ClientService } from "./client.service";
