15cec1e5e635528c2e5f7da41d239ed4ad6bb6f2
Baraka Mulumia <barakamulumia@gmail.com>
2021-02-19 21:37:48 +0300

    +utility to calculate the price based on  distance

---
+++ client/src/Resources/utils/price.js
@@ -0,0 +1,15 @@
+export default function calcPriceFromLatLng(pickup, destination) {
+  const R = 6371e3; //Radius of the earth in metres
+
+  const φ1 = (pickup.latlng.lat * Math.PI) / 180; // φ, λ in radians
+  const φ2 = (destination.latlng.lat * Math.PI) / 180;
+  const Δφ = ((destination.latlng.lat - pickup.latlng.lat) * Math.PI) / 180;
+  const Δλ = ((pickup.latlng.lng - destination.latlng.lng) * Math.PI) / 180;
+
+  const a =
+    Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+    Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+  return Math.round(((R * c) / 10) * 100) / 100;
+}
