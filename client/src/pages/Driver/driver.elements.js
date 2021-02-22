b54485b49ea395a282a1778ec5fe8d5b3ae86324
Baraka Mulumia <barakamulumia@gmail.com>
2021-02-22 13:21:28 +0300

    Fixed Background

--- client/src/pages/Driver/driver.elements.js
+++ client/src/pages/Driver/driver.elements.js
@@ -5,4 +5,7 @@ export const Box = styled(Grid)`
   background: url("https://i.postimg.cc/DZqKmp8k/highway-truck.jpg");
   background-size: cover;
   min-height: 100vh;
+  background-position: center;
+  background-attachment: fixed;
+  padding-bottom: 30px;
 `;
