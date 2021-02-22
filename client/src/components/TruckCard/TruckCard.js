23d0acd09acce65467cfab0a22e3473998cf3407
Baraka Mulumia <barakamulumia@gmail.com>
2021-02-22 13:22:31 +0300

    pickup and destination dependancy for display

--- client/src/components/TruckCard/TruckCard.js
+++ client/src/components/TruckCard/TruckCard.js
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { useSelector } from "react-redux";
+import { selectPickUp, selectDestination } from "../../state/maps.slice";
 import { Image } from "../index";
 import { CardContainer, Timeline } from "./TruckCard.elements";
 
 const TruckCard = () => {
-  return (
-    <CardContainer>
-      <Image
-        src="https://i.postimg.cc/vHRBfYDN/car-driver.jpg"
-        alt="truck driver image"
-      />
-      <Timeline>7 mins away</Timeline>
-    </CardContainer>
-  );
+  const pickup = useSelector(selectPickUp);
+  const destination = useSelector(selectDestination);
+
+  let content;
+
+  if (pickup && destination) {
+    content = (
+      <CardContainer>
+        <Image
+          src="https://i.postimg.cc/vHRBfYDN/car-driver.jpg"
+          alt="truck driver image"
+        />
+        <Timeline>7 mins away</Timeline>
+      </CardContainer>
+    );
+  } else {
+    content = null;
+  }
+
+  return <Fragment>{content}</Fragment>;
 };
 
 export default TruckCard;
