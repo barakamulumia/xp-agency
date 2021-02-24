c6c2a5fc6ae4fe72c430154ee9dcbbf73a833a4f
Baraka Mulumia <barakamulumia@gmail.com>
2021-02-24 06:54:23 +0300

    Admin 101

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,32 +1,15 @@
-import { Admin, Resource, ListGuesser, fetchUtils } from "react-admin";
-import { stringify } from "querystring";
-
-const apiUrl = "http://localhost:8080/api";
-const httpClient = fetchUtils.fetchJson;
-
-const dataProvider = {
-  getList: (resource, params) => {
-    const { page, perPage } = params.pagination;
-    const { field, order } = params.sort;
-    const query = {
-      sort: JSON.stringify([field, order]),
-      range: JSON.stringify([(page - 1) * perPage, page * perPage - 1]),
-      filter: JSON.stringify(params.filter),
-    };
-    const url = `${apiUrl}/${resource}?${stringify(query)}`;
-
-    return httpClient(url).then(({ headers, json }) => ({
-      data: json.map((resource) => ({
-        ...resource,
-      })),
-      total: parseInt(headers.get("content-range"), 10),
-    }));
-  },
-};
+import { Admin, Resource, ListGuesser } from "react-admin";
+import { ClientList, EditClient, CreateClient } from "./compnents/clients";
+import { dataProvider } from "./dataProvider";
 
 const App = () => (
   <Admin dataProvider={dataProvider}>
-    <Resource name="clients" list={ListGuesser} />
+    <Resource
+      name="clients"
+      list={ClientList}
+      edit={EditClient}
+      create={CreateClient}
+    />
     <Resource name="drivers" list={ListGuesser} />
   </Admin>
 );
